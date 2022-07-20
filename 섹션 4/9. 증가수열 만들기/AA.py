d481dcd9bf6d89b00c85a821931d993ae633bab0
Jesper (Sung Hyun) Hong <seonghyun91@gmail.com>
2022-07-20 16:01:36 +0900

    .

--- 섹션 4/9. 증가수열 만들기/AA.py
+++ 섹션 4/9. 증가수열 만들기/AA.py
@@ -13,30 +13,30 @@ res = ""
 prev = 0  # 이전에 추출한 항보다는 커야 하니까
 tmp = []
 
-# while lt <= rt:
-#     if a[lt] > prev:
-#         tmp.append(  (a[lt], "L")   )
-#     if a[rt] > prev:
-#         tmp.append(  (a[rt], "R")   )
-#
-#     tmp.sort()   #튜플[0] 기준으로 정렬
-#
-#     if len(tmp) == 0:  # 더 이상 가져올 자료가 없음.
-#         break
-#
-#     else:         #두 튜플 중 첫 자료
-#         res += tmp[0][1]
-#                      #그 중 알파벳
-#         prev = tmp[0][0]
-#
-#         if tmp[0][1] == 'L':
-#             lt +=1
-#         else:
-#             rt -=1
-#     tmp.clear()
-#
-# print(len(res))
-# print(res)
+while lt <= rt:
+    if a[lt] > prev:
+        tmp.append(  (a[lt], "L")   )
+    if a[rt] > prev:
+        tmp.append(  (a[rt], "R")   )
+
+    tmp.sort()   #튜플[0] 기준으로 정렬
+
+    if len(tmp) == 0:  # 더 이상 가져올 자료가 없음.
+        break
+
+    else:         #두 튜플 중 첫 자료
+        res += tmp[0][1]
+                     #그 중 알파벳
+        prev = tmp[0][0]
+
+        if tmp[0][1] == 'L':
+            lt +=1
+        else:
+            rt -=1
+    tmp.clear()
+
+print(len(res))
+print(res)
 
 
 #아래 답은 time limit 발생
