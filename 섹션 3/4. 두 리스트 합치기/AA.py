31f6f470795e19c03d3cb68b86422ba874e8615d
Jesper (Sung Hyun) Hong <seonghyun91@gmail.com>
2022-07-21 15:56:30 +0900

    .

--- 섹션 3/4. 두 리스트 합치기/AA.py
+++ 섹션 3/4. 두 리스트 합치기/AA.py
@@ -1,8 +1,21 @@
-n = [1, 2, 3, 5]
+# n = [1, 2, 3, 5]
+#
+# m = [3, 7]
 
-m = [3, 7]
+import sys
+import time
 
-i =j = 0
+start_time = time.time()
+
+# i = input()
+# n = list(map(int, input().split()))
+# j = input()
+# m = list(map(int, input().split()))
+
+n= [1, 10, 27, 39, 50, 61, 65, 70, 93, 93]
+m = [7, 51, 65, 66, 70, 82, 93]
+
+i = j = 0
 res = []
 
 
@@ -20,4 +33,26 @@ if i < len(n):
 if j < len(m):
     res = res + m[j:]
 
-print(res)
+
+#pop보다 위가 빠르네
+# res = []
+# for i in range(len(n)):
+#     for j in range(len(m)):
+#         while i < len(n) and j < len(m):
+#             if n[i] < m[j]:
+#                 k = n.pop(0)
+#             else:
+#                 k = m.pop(0)
+#             res.append(k)
+#
+# if n:
+#     res += n
+# else:
+#     res += m
+#
+# for x in res:
+#     print(x)
+
+end_time = time.time()
+
+print(end_time - start_time)
