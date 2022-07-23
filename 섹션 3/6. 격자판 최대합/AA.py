cd75668e286d5f15fe71ade24a9d4b7aecf6c9bc
Jesper (Sung Hyun) Hong <seonghyun91@gmail.com>
2022-07-23 16:18:29 +0900

    fixed indentation err

--- 섹션 3/6. 격자판 최대합/AA.py
+++ 섹션 3/6. 격자판 최대합/AA.py
@@ -28,15 +28,16 @@ matrix = []
 a = [list(map(int, input().split())) for _ in range(n)]
 
 largest = -2147000000
+sum4=sum4=0
 for i in range(n):
     sum1 = sum2 = 0
     for j in range(n):
         sum1 += a[i][j]
         sum2 += a[j][i]  # 옼ㅋㅋ 그냥 각 열과 각 행의 합을 바로 매번 비교
-        if sum1 > largest:
-            largest = sum1
-        if sum2 > largest:
-            largest = sum2
+    if sum1 > largest:
+        largest = sum1
+    if sum2 > largest:
+        largest = sum2
 
 sum1 = sum2 = 0
 for i in range(n):             # 한줄에 한 값씩만 더하며 내려오면 될테니...
@@ -48,3 +49,5 @@ for i in range(n):             # 한줄에 한 값씩만 더하며 내려오면 
         largest = sum2
 
 print(largest)
+
+print(sum1,sum2)
