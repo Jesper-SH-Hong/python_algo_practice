1ba1955cc25be9f1762a6202d2afdaecb4d71a32
Jesper (Sung Hyun) Hong <seonghyun91@gmail.com>
2022-07-25 11:02:37 +0900

    fixed indentation err

--- 섹션 3/10. 스도쿠 검사/AA.py
+++ 섹션 3/10. 스도쿠 검사/AA.py
@@ -1,21 +1,23 @@
 def check(a):
-    ch = [0] * 10
     for i in range(9):
+        ch1 = [0] * 10  # ch = check
+        ch2 = [0] * 10
         for j in range(9):
             ch1[a[i][j]] = 1
             ch2[a[j][i]] = 1
-            if sum1(ch1) != 9 or sum(ch2) != 9:
-                return False
+        if sum(ch1) != 9 or sum(ch2) != 9:
+            return False
 
-        else:
+    for i in range(3):
+        for j in range(3):
+            ch3 = [0] * 10
             for k in range(3):
                 for s in range(3):
                     ch3[a[3 * i + k][3 * j + s]] = 1
 
-                    if sum(ch3) != 9:
-                        return False
-            else:
-                return True
+            if sum(ch3) != 9:
+                return False
+    return True
 
 
 a = [list(map(int, input().split())) for _ in range(9)]
--- 섹션 3/11. 격자판 회문수/AA.py
+++ 섹션 3/11. 격자판 회문수/AA.py
@@ -15,12 +15,14 @@ board = [list(map(int, input().split())) for _ in range(7)]
 
 cnt = 0
 for i in range(3):   #사이즈가 5인 palindrome을 찾으니 그냥 종/횡 다 0,1,2에서만 시작하면 됨.
+
+    #행처리
     for j in range(7): #7행
         tmp=board[j][i:i+5]   #j는 행. i는 5개를 솎아내는 역할
         if tmp ==tmp[::-1]:
             cnt+=1
 
-
+        #열처리
         for k in range(2): # 회문 길이 5니까 5//2만 체크.    세로..는 이렇게
             if board[i+k][j] != board[i+5-k-1][j]:
                 break
