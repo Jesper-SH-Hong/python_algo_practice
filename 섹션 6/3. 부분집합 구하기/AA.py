76e916c09ae1b75b32d6c04791729507dce32818
Jesper (Sung Hyun) Hong <seonghyun91@gmail.com>
2022-07-30 11:40:33 +0900

    ...

--- 섹션 6/3. 부분집합 구하기/AA.py
+++ 섹션 6/3. 부분집합 구하기/AA.py
@@ -1,20 +1,39 @@
 import sys
-#sys.stdin=open("input.txt", "r")
+
+
+# sys.stdin=open("input.txt", "r")
+
+# n =    1      2      3
+# sub = o/x -> o/x -> o/x  -> 이렇게 탐색. 총 8가지(공집합 포함)
+
+
+#             D(1)                                      #이렇게 사용하네/안하네 뻗어나간 게 상태트리
+#        o /       \ x
+#       D(2)        D(2)
+#     o /  \x     o/   \x
+#    D(3)
+#   o/ \x
+#  D(4) D(4)          ..말단 if문에 걸려 return할 것.
+#  123   12
+
+
 def DFS(v):
-    if v==n+1:
-        for i in range(1, n+1):
-            if ch[i]==1:
+    if v == n + 1:              #종착점 v가 말단 노드까지 왔을 떄. 아래와 같이 작업하겠소.
+        for i in range(1, n + 1):
+            if ch[i] == 1:
                 print(i, end=' ')
         print()
     else:
-        ch[v]=1
-        DFS(v+1)
-        ch[v]=0
-        DFS(v+1)
+        ch[v] = 1     #이값을 사용.
+        DFS(v + 1)                          #D1 o->2 o ->3 o ->4
+        ch[v] = 0     #이값을 사용 안할래.
+        DFS(v + 1)                          #D(3) x -> D(4)
 
-if __name__=="__main__":
-    n=int(input())
-    ch=[0]*(n+1)
+
+if __name__ == "__main__":
+    # n=int(input())
+    n = 3
+    ch = [0] * (n + 1)  # 어떨떄 사용했나 안했나 확인할 체크 변수.
+
     DFS(1)
-    
 
--- 섹션 6/4. 합이 같은 부분집합/AA.py
+++ 섹션 6/4. 합이 같은 부분집합/AA.py
@@ -1,21 +1,51 @@
 import sys
-#sys.stdin=open("input.txt", "r")
-def DFS(L, sum):
-    if sum>total//2:
-        return
-    if L==n:
-        if sum==(total-sum):
-            print("YES")
-            sys.exit(0)
-    else:
-        DFS(L+1, sum+a[L])
-        DFS(L+1, sum)
 
-if __name__=="__main__":
-    n=int(input())
-    a=list(map(int, input().split()))
-    total=sum(a)
+# sys.stdin=open("input.txt", "r")
+
+# n개의 원소인 자연수 집합
+# 두 부분집합을 나눠서 합이 같은 경우가 있으면 YES, 아니면 NO 출력
+
+
+# 1을 부분집합에 넣는다/만다
+# 3을 ...
+
+# 토탈 2 **6 -1(공집합) 경우의 수
+
+
+a = 1, 3, 5, 6, 7, 10
+
+
+# DFS(L, sum)   #L:a의 인덱스 번호이자 동시에 Level   #sum: 부분집합의 합
+
+# idx 0  1  2  3  4  5
+# a = 1, 3, 5, 6, 7, 10
+
+#                 D(0,0)
+#               /       \
+#           D(1,1)      D(1,0)        왼쪽: 0번인덱스(값:1)을 부분집합에 사용 <-> 오른쪽: 안사용
+#            /  \        /  \
+#      D(2,4)   D(2,1)                  sum: 1+3   vs  1
+#      /   \
+# D(3,9)   D(3,4)                     #역시 끝은 L이 n이 되어야 끝날 것.(idx 0부터 시작했으니 5레벨까지 n레벨 채워지고  6레벨(n)에 도달해야 leaf)
+
+
+def DFS(L, sum2):
+    if sum2 > total // 2:         #없어도 무관이지만, 시간복잡도 줄이기. (아, 아예 total이 홀수냐고 물어봐도..?)
+        return
+    if L == n:   #6렙 도달
+        if sum2 == (total - sum2):  # 부분집합1에 참여하지 않은 원소들의 합, 즉 부분집합2의 합이랑 같냐.
+            print("YES")
+            sys.exit(0) #플그램 종료
+    else:
+        DFS(L + 1, sum2 + a[L])
+        DFS(L + 1, sum2)
+
+
+if __name__ == "__main__":
+    # n = int(input())
+    # a = list(map(int, input().split()))
+    n = 6
+    a = 1, 3, 5, 6, 7, 10
+    total = sum(a)
     DFS(0, 0)
     print("NO")
-
-    
